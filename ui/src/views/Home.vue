83cf6f8455fb2e694ec2c5482875c10e8c549469
Lakshmi Sashank Gandodi <sashank.gandodi@copart.com>
2024-05-08 19:47:47 +0530

    Setup Vue Project in ui folder

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex h-screen items-center justify-center">
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="mb-2 text-9xl text-indigo-600">Berry</h1>
+            <h1
+                class="text-xl font-light italic tracking-widest text-indigo-600"
+            >
+                Grow Fruitful Connections!
+            </h1>
+        </div>
+    </div>
+</template>
