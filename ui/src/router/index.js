ff3c656264ccada9e15f834456bfaae1ad84b378
Iskak06 <isf200611@gmail.com>
2024-05-14 19:58:53 +0200

    added Test page - Iskak

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Register from "@/views/auth/Register.vue";
 import Login from "@/views/auth/Login.vue";
+import Test from "@/views/Test.vue";
 import { isAuthenticated } from '@/services/auth.js';
 
 const router = createRouter({
@@ -19,6 +20,10 @@ const router = createRouter({
     {
       path: "/login",
       component: Login,
+    },
+    {
+      path: "/test",
+      component: Test,
     },
   ],
 });
